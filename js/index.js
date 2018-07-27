6573a34ad3cb179ac398d763c59f62a0ccf2b058
药王 <498792539@qq.com>
2018-07-27 10:34:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,113 +1,113 @@
 
 window.onload = () => {
     var menu = [
-                    {
-                        name:'前端',
-                        subs:[
-                            {
-                                name:'javascript 通用',
-                                subs:[
-                                    {
-                                        name:'包装对象',
-                                        src:'./doc/frontend/javascript/bzdx.md'
-                                    },
-                                    {
-                                        name:'原型链',
-                                        src:'./doc/frontend/javascript/prototype.md'
-                                    },
-                                    {
-                                        name:'this',
-                                        src:'./doc/frontend/javascript/this.md'
-                                    },
-                                    {
-                                        name:'call | apply | bind',
-                                        src:'./doc/frontend/javascript/cab.md'
-                                    },
-                                    {
-                                        name:'javascript 特殊技巧 (待补充)',
-                                        src:'./doc/frontend/javascript/spec_tech.md'
-                                    },
-                                    {
-                                        name:'sublime 按照和使用技巧',
-                                        src:'./doc/frontend/javascript/sublime.md'
-                                    },
-                                    {
-                                        name:'测试文档',
-                                        src:'./doc/frontend/javascript/test.md'
-                                    }
-                                ]
-                            },
-                            {
-                                name:'WebGL',
-                                subs:[
-                                    {
-                                        name:'[threejs入门-1] hello,Three.js',
-                                        src:'./doc/frontend/webgl/hello.md'
-                                    },
-                                    {
-                                        name:'[threejs入门-2] 创造一个世界',
-                                        src:'./doc/frontend/webgl/Building_a_world.md'
-                                    },
-                                    {
-                                        name:'[threejs入门-3] 探索和互动',
-                                        src:'./doc/frontend/webgl/Exploring_and_Interacting.md'
-                                    }
-                                ]
-                            },
-                            {
-                                name:'javascript 性能优化',
-                                subs:[
-                                    {
-                                        name:'javascript 高性能（阅读总结）',
-                                        src:'./doc/frontend/Performance/HPjavascript.md'
-                                    }
-                                ]
-                            },
-                            {
-                                name:'jquery',
-                                subs:[
-                                    {
-                                        name:'犀利 jquery （阅读总结-待补充）',
-                                        src:'./doc/frontend/jquery/xilijquery.md'
-                                    }
-                                ]
-                            },
-                            {
-                                name:'html',
-                                subs:[
-                                    {
-                                        name:'test',
-                                        src:'./1.md'
-                                    }
-                                ]
-                            },
-                            {
-                                name:'css',
-                                subs:[
-                                    {
-                                        name:'test',
-                                        src:'./1.md'
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        name:'后端',
-                        subs:[
-                            {
-                                name:'C++',
-                                subs:[
-                                    {
-                                        name:"test",
-                                        src:"./1.md"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ];
-    
+        {
+            name: '前端',
+            subs: [
+                {
+                    name: 'javascript 通用',
+                    subs: [
+                        {
+                            name: '包装对象',
+                            src: './doc/frontend/javascript/bzdx.md'
+                        },
+                        {
+                            name: '原型链',
+                            src: './doc/frontend/javascript/prototype.md'
+                        },
+                        {
+                            name: 'this',
+                            src: './doc/frontend/javascript/this.md'
+                        },
+                        {
+                            name: 'call | apply | bind',
+                            src: './doc/frontend/javascript/cab.md'
+                        },
+                        {
+                            name: 'javascript 特殊技巧 (待补充)',
+                            src: './doc/frontend/javascript/spec_tech.md'
+                        },
+                        {
+                            name: 'sublime 按照和使用技巧',
+                            src: './doc/frontend/javascript/sublime.md'
+                        },
+                        {
+                            name: '测试文档',
+                            src: './doc/frontend/javascript/test.md'
+                        }
+                    ]
+                },
+                {
+                    name: 'WebGL',
+                    subs: [
+                        {
+                            name: '[threejs入门-1] hello,Three.js',
+                            src: './doc/frontend/webgl/hello.md'
+                        },
+                        {
+                            name: '[threejs入门-2] 创造一个世界',
+                            src: './doc/frontend/webgl/Building_a_world.md'
+                        },
+                        {
+                            name: '[threejs入门-3] 探索和互动',
+                            src: './doc/frontend/webgl/Exploring_and_Interacting.md'
+                        }
+                    ]
+                },
+                {
+                    name: 'javascript 性能优化',
+                    subs: [
+                        {
+                            name: 'javascript 高性能（阅读总结）',
+                            src: './doc/frontend/Performance/HPjavascript.md'
+                        }
+                    ]
+                },
+                {
+                    name: 'jquery',
+                    subs: [
+                        {
+                            name: '犀利 jquery （阅读总结-待补充）',
+                            src: './doc/frontend/jquery/xilijquery.md'
+                        }
+                    ]
+                },
+                {
+                    name: 'html',
+                    subs: [
+                        {
+                            name: 'test',
+                            src: './1.md'
+                        }
+                    ]
+                },
+                {
+                    name: 'css',
+                    subs: [
+                        {
+                            name: 'test',
+                            src: './1.md'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: '后端',
+            subs: [
+                {
+                    name: 'C++',
+                    subs: [
+                        {
+                            name: "test",
+                            src: "./1.md"
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+
     initializeContent(menu);
 }
 
@@ -118,7 +118,7 @@ function initializeContent(menu) {
             return {
                 input: '',
                 isCollapse: true,
-                tops:menu
+                tops: menu
             }
         },
         methods: {
@@ -143,7 +143,7 @@ function initializeContent(menu) {
                 // 读取 markdown 文件并且修改内容
                 var xhr = new XMLHttpRequest();
                 var nowTime = new Date().getTime();//获取当前时间作为随机数
-                var url = filename +'?time='+ nowTime;
+                var url = filename + '?time=' + nowTime;
                 xhr.open('get', url, true);
                 xhr.send();
                 xhr.onreadystatechange = function () {
